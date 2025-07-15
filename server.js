e3dada57de562bbbaa23da312cdb48f1d4e8125b
Sandeepmothe <mothesandeep92@gmail.com>
2025-07-15 10:06:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,12 +7,14 @@ require('dotenv').config();
 
 
 const app = express();
+const PORT = process.env.PORT || 5000;
+
 app.use(cors());
 app.use(express.json());
 app.use('/api/reviews', reviewRoutes);
 
 sequelize.sync().then(() => {
-  app.listen(5000, () => {
+  app.listen(PORT, () => {
     console.log('Server running at http://localhost:5000');
   });
 });
